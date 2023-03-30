c3daf5c7734d7e881760f3240fa7f1cc9adff68e
minaripenguin <minaripenguin@users.noreply.github.com>
2023-03-30 12:51:00 +0800

    OrchidOs Prefrences import back port from a14
    util: Import some utils from LineageOS
    
    * FileUtils, ImageUtils and ScreenType
    
    Change-Id: I689c22c822f6d89301e4f24924a0966d75b2f376
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat80@gmail.com>
    
    util: Add PackageManagerUtils
    
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat80@gmail.com>
    Change-Id: I2aaf83f1180063ff28be4cccadd7b9aca103cc95
    
    support: Import RemotePreference from LineageOS
    
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: Ieac9e293c9f3d29a3c7911b2ce55b7f1f21f9016
    
    Rework preferences
    
    Don't crash if default is not provided
    
    Change-Id: Ic745c0e12790a1ff856df927170ed9a8e2d4583f
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    
    preferences: Introduce {Global,Secure,System}MainSwitchPreference
    
    Change-Id: I04cf71ea60b3ecde3c8be039ab320e8730ca4b3f
    
    support: Import FileUtils and ScreenType as lineageos internal
    
    * For backward compatibility
    
    Change-Id: Ib42921e8d44e7b5811714f76d749253aa3c0d63b
    
    support: Update ListPreferences from LineageOS
    
    Change-Id: I148c2b8009259f2c349a938286187d7525c5a447
    
    support: Add SystemSettingDropDownPreference from lineageos
    
    Change-Id: I71998d4d814ea982c85b91a52ff0a11ca473e92d
    
    support: Add requiresService attribute
    
    Change-Id: I953444a53cd892d8e70941487709964fd671576a
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    
    support: Add Cardview Preference
    
    The java implemention of our preference is based on DU's CustomPreference.
    
    Layout is as per the design by Harsh V23 (https://plus.google.com/+HarshV23) harshv23
    
    aex edits: * Make it cardview
               * Change layout according to the design
               * Expose dimens of CardView
    
    Change-Id: Ifa3f2275689d15a79e979b6d05c23db59483db85
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    
    support: Introduce ProperSeekBarPreference
    
    let's make nice and clear, proper seekbar preference.
    finally
    
    VibratorStrengthPreference included as sample of subclass implementation
    just define attrs in constructor and override changeValue
    
    Attrs:
    // xmlns:app="http://schemas.android.com/apk/res-auto"
    app:interval (integer) [1] seek step, also used for plus/minus clicks
    app:showSign (boolean) [false] show "+" sign for positive values
    app:units (string) [] units, at right side of value (take care about leading space if needed)
    app:continuousUpdates (boolean) [false] update value continuous while tracking thumb
    // xmlns:android="http://schemas.android.com/apk/res/android"
    android:min (integer) [0] minimal value
    android:max (integer) [100] maximal value
    android:defaultValue (integer) [] default value (can be absent, but reset to default button also absent in such case)
    
    ZeNiXxX: Adapt to AndroidX
    
    Change-Id: Ic0f99e55b1519596e49ff2af14895cea1a19c764
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    
    ProperSeekBarPreference: Fix auto reset in few cases
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    ProperSeekBarPreference: Improve layout
    
    ProperSeekBarPreference: Fix displaying units
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    ProperSeekBarPreference: Add missing methods
    
    support: Extend ProperSeekBarPreference
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    support: Add remaining list settings preferences
    
    Taken from AICP
    
    Change-Id: Ibe3454b7841e39487529a4004727cd941c4c72fd
    
    support: Add SecureSettingIntListPreference to the collection
    
    Change-Id: I2c2c44322796b501e2828b41091bc0d1429556ea

---
+++ src/com/android/settings/custom/preference/CustomSystemSeekBarPreference.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ package com.android.settings.custom.preference;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.provider.Settings;
+
+public class CustomSystemSeekBarPreference extends CustomSeekBarPreference {
+
+    public CustomSystemSeekBarPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public CustomSystemSeekBarPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public CustomSystemSeekBarPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    protected boolean isPersisted() {
+        return Settings.System.getString(getContext().getContentResolver(), getKey()) != null;
+    }
+
+    protected boolean getBoolean(String key, boolean defaultValue) {
+        return Settings.System.getInt(getContext().getContentResolver(),
+                key, defaultValue ? 1 : 0) != 0;
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restorePersistedValue, Object defaultValue) {
+        final boolean checked;
+        if (!restorePersistedValue || !isPersisted()) {
+            if (defaultValue == null) {
+                return;
+            }
+            checked = (boolean) defaultValue;
+            if (shouldPersist()) {
+                persistBoolean(checked);
+            }
+        } else {
+            // Note: the default is not used because to have got here
+            // isPersisted() must be true.
+            checked = getBoolean(getKey(), false /* not used */);
+        }
+        setChecked(checked);
+    }
+}
